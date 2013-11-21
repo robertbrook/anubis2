809ff41d58b1300a6753370ba9f47230d78b367c
Robert Brook <mail@robertbrook.com>
2013-11-21 11:30:15 +0000

    testing

--- test/test.js
+++ test/test.js
@@ -1,1 +1,20 @@
-// dunno
+var app = require('../app');
+var superagent = require('superagent');
+var should = require('should');
+var agent = superagent.agent();
+
+
+
+describe('Suite one', function(){
+        
+    it('should redirect to dashboard if user is logged in', function(done) {
+    
+      agent.get('localhost:3000').end(function(err, res) {
+        res.should.have.status(200);
+        res.text.should.include('Energy');
+        return done();
+      });
+
+    
+  });
+});
